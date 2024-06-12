25bd43b528d2647e99ed5c7b6ed96b70b6c66dcf
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-06-12 16:36:07 +0300

    add js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,16 +2,78 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
+const refs = {
+    inputDate: document.querySelector('input#datetime-picker'),
+    btnStart: document.querySelector('[data-start]'),
+    day: document.querySelector('[data-days]'),
+    hour: document.querySelector('[data-hours]'),
+    minute: document.querySelector('[data-minutes]'),
+    second: document.querySelector('[data-seconds]'),
+}
 
-const options = {
+let userData;
+refs.btnStart.setAttribute('disabled', true);
+const timer = {
+    intervalId: null,
+    // isActive: falce,
+    start() {
+        // if (this.isActive) {
+        //     return;
+        // }
+        // const startTime = Date.now();
+        // this.isActive = true;
+
+        this.intervalId = setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = userData - currentTime;
+            // console.log(deltaTime);
+            timeComponents = convertMs(deltaTime);
+            const { days, hours, minutes, seconds } = timeComponents;
+            refs.day.textContent = days;
+            refs.hour.textContent = hours;
+            refs.minute.textContent = minutes;
+            refs.second.textContent = seconds;
+            refs.inputDate.setAttribute('disabled', '');
+            if (deltaTime < 1000) {
+                Notiflix.Notify.success('Timer is over');
+                refs.inputDate.removeAttribute('disabled');
+                clearInterval(this.intervalId);
+            }
+        }, 1000);
+
+        refs.btnStart.setAttribute('disabled', true);
+    },
+
+    // stop() {
+    //     clearInterval(this.intervalId);
+    //     this.isActive = false;
+    // }
+};
+// timer.start();
+
+function pad(value) {
+    return String(value).padStart(2, '0')
+}
+
+
+const calendarEl = flatpickr(refs.inputDate, {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
+        // console.log(selectedDates[0]);
+        if (selectedDates[0] < Date.now()) {
+            Notiflix.Notify.failure('Please choose a date in the future');
+            refs.btnStart.setAttribute('disabled', true);
+        } else {
+            userData = selectedDates[0].getTime();
+            refs.btnStart.removeAttribute('disabled');
+
+            refs.btnStart.addEventListener('click', timer.start);
+        }
     },
-};
+});
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
@@ -21,17 +83,14 @@ function convertMs(ms) {
     const day = hour * 24;
 
     // Remaining days
-    const days = Math.floor(ms / day);
+    const days = pad(Math.floor(ms / day));
     // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
+    const hours = pad(Math.floor((ms % day) / hour));
     // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
     return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
